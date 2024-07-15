24f194c6a24fa21fa292916311da2b7f20268a2b
Sümeyye Buran <145922575+sumeyyeburan@users.noreply.github.com>
2024-07-15 20:15:05 +0300

    initial commit

--- div.asm
+++ div.asm
@@ -1,4 +1,4 @@
-;  Write a program to divide unsigned value ABh (171d) 
+; Write a program to divide unsigned value ABh (171d) 
 ;by 0Ah (10d). Then save the quotient to the memory 
 ;address 0100:0500h, and remainder to the memory address 
 ;0100:0502h.
